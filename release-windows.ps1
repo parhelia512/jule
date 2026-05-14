18977276d5ad648b8ed6949430eab81e3df7ef1e
mertcandav <mertcandav@outlook.com>
2026-05-15 02:27:28 +0300

    release: use julec-ir compile commands in release process of windows

--- release-windows.ps1
+++ release-windows.ps1
@@ -16,7 +16,7 @@ New-Item -ItemType Directory -Path ".\bin" -Force
 
 # Get ARM64 IRs from the GitHub and compile it.
 curl.exe -L -o ir.cpp https://raw.githubusercontent.com/julelang/julec-ir/main/src/windows-arm64.cpp
-clang++ -static -Wno-everything --std=c++20 -fwrapv -ffloat-store -fno-fast-math -fno-rounding-math -ffp-contract=fast -O3 -flto=thin -fuse-ld=lld -DNDEBUG -fomit-frame-pointer -fno-strict-aliasing -o .\bin\julec.exe ir.cpp -lws2_32 -lshell32 -liphlpapi -lsynchronization
+curl.exe -fsSL https://raw.githubusercontent.com/julelang/julec-ir/refs/heads/main/meta/windows-arm64.txt | Invoke-Expression
 
 # ZIP it.
 Set-Location -Path ".."
@@ -29,7 +29,7 @@ Remove-Item ".\ir.cpp"
 
 # Get AMD64 IRs from the GitHub and compile it.
 curl.exe -L -o ir.cpp https://raw.githubusercontent.com/julelang/julec-ir/main/src/windows-amd64.cpp
-x86_64-w64-mingw32-clang++ -static -Wno-everything --std=c++20 -fwrapv -ffloat-store -fno-fast-math -fno-rounding-math -ffp-contract=fast -O3 -flto=thin -fuse-ld=lld -DNDEBUG -fomit-frame-pointer -fno-strict-aliasing -o .\bin\julec.exe ir.cpp -lws2_32 -lshell32 -liphlpapi -lsynchronization
+curl.exe -fsSL https://raw.githubusercontent.com/julelang/julec-ir/refs/heads/main/meta/windows-amd64.txt | Invoke-Expression
 
 # ZIP it.
 Set-Location -Path ".."
